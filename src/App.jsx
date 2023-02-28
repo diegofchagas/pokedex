008c80c9b0764ccd08612fc0decf751a4fdddb76
Diego Chagas <diego.chagas@afya.com.br>
2023-02-28 14:31:31 -0300

    array adicionano para receber 150 pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
-import { useState,useEffect } from 'react'
-import axios from 'axios'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [pokemons, setPokemons] = useState({})
+  const [pokemons, setPokemons] = useState({});
 
-  async function getPokemons(id){
-   await axios.get( `https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(res => { const pokemon = res.data
-      setPokemons(pokemons => ({...pokemons, [id]:pokemon}))
-    })
-    .catch(error => console.log(error))
-
-
+  async function getPokemons(id) {
+    await axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((res) => {
+        const pokemon = res.data;
+        setPokemons((pokemons) => ({ ...pokemons, [id]: pokemon }));
+      })
+      .catch((error) => console.log(error));
   }
 
-  useEffect(()=>{
-    getPokemons(25)
-  },[])
+  const allPokemons =()=> Array(150).fill().map((_,index) => getPokemons(index + 1))
 
-  
-
+  useEffect(() => {
+    allPokemons()
+  }, []);
 
   return (
     <div>
-       <h1>Pokedex</h1>
-        <ul>
-          {Object.values(pokemons).map(({id,name,sprites,types})=> <li>
+      <h1>Pokedex</h1>
+      <ul>
+        {Object.values(pokemons).map(({ id, name, sprites, types }) => (
+          <li>
             <img src={sprites.front_default} alt="pokemons" />
-            <h2>{id}. {name}</h2>
-            <p>{types.map(({type})=> type.name).join(" || ")}</p>
-            </li>)}
-        </ul>
-
+            <h2>
+              {id}. {name}
+            </h2>
+            <p>{types.map(({ type }) => type.name).join(" || ")}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
