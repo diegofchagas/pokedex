5e379b00415489c3d0c9003c0fb04c5d771654f9
Diego Chagas <diego.chagas@afya.com.br>
2023-02-28 14:25:35 -0300

    requisicao com axios feita

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState({})
 
   async function getPokemons(id){
    await axios.get( `https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(res => console.log(res))
+    .then(res => { const pokemon = res.data
+      setPokemons(pokemons => ({...pokemons, [id]:pokemon}))
+    })
     .catch(error => console.log(error))
 
 
@@ -17,11 +19,13 @@ function App() {
     getPokemons(25)
   },[])
 
+  console.log(pokemons)
+
 
   return (
     <div>
-      
-
+       <h1>Pokedex</h1>
+        
 
     </div>
   )
