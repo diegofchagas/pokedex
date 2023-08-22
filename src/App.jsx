a243fdcff546f2760806e52bc5618210ba1613dc
Diego Chagas <diego.chagas@afya.com.br>
2023-08-22 18:57:01 -0300

    alterando estrutura do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,74 @@
-import { useState, useEffect } from "react";
-import {MagnifyingGlass} from 'phosphor-react'
+import { useEffect, useState } from "react";
+import {MagnifyingGlass} from "@phosphor-icons/react"
 import axios from "axios";
 import "./App.css";
 
 function App() {
   const [pokemons, setPokemons] = useState({});
-  const [busca, setBusca] = useState("")
+  const [busca, setBusca]= useState('')
 
- 
+  const getPokemons = async (id) => {
+
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      setPokemons((prevPokemons) => ({ ...prevPokemons, [id]: response.data }));
+  } catch (error) {
+    console.log(error)
+  }
+  };
+
+  const arrayPokemons = () =>
+    Array(150)
+      .fill()
+      .map((_, index) => getPokemons(index + 1));
+
+  const buscarPokemons = Object.values(pokemons).filter(
+    (pokemon) => pokemon.name.toLocaleLowerCase().includes(busca.toLocaleLowerCase()) ||
+    pokemon.id === parseInt(busca)
+  )
+
   
 
-  async function getPokemons(id) {
-    await axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      .then((res) => {
-        const pokemon = res.data;
-        setPokemons((pokemons) => ({ ...pokemons, [id]: pokemon }));
-      })
-      .catch((error) => console.log(error));
-  }
-
-  const allPokemons =()=> Array(150).fill().map((_,index) => getPokemons(index + 1))
-
   useEffect(() => {
-    allPokemons()
+    arrayPokemons();
   }, []);
 
-  const pokemonsBusca = Object.values(pokemons).filter(
-   (pokemon)  => 
-    pokemon.name.toLocaleLowerCase().includes(busca.toLocaleLowerCase()) ||
-    pokemon.id === parseInt(busca)
-    )
 
   return (
     <div className="container">
       <h1>Pokedex</h1>
 
       <div className="busca-container">
-      <MagnifyingGlass size={40}/>
+      <MagnifyingGlass size={40} />
         <input 
         className="busca"
-        type="search" 
-        placeholder="pesquisar pokemons" 
+        type="search"
+        placeholder="pesquisar pokemons"
         value={busca}
-        onChange={({target})=> setBusca(target.value)} 
+        onChange={({target})=> setBusca(target.value)}
+
         />
+
       </div>
 
       <ul className="pokemons">
-        {pokemonsBusca.map(({ id, name, types }) => (
-          <li className={`card ${types[0].type.name}`} key={id}>
-            <img className="card-image" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} alt="pokemons" />
+        {buscarPokemons.map(({ id, name, types }) => (
+          <li className={`card ${types[0].type.name}`}>
+            <img
+              className="card-image"
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+              alt={name}
+            />
             <h2>
               {id}. {name}
             </h2>
-            <p className="tipo">{types.map(({ type }) => type.name).join(" || ")}</p>
+            <p className="type">{types.map((item) => item.type.name).join(" || ")}</p>
           </li>
         ))}
       </ul>
-
-     
-     
     </div>
   );
 }
 
 export default App;
+
